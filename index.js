6b47548f703512950112ce43d8f57dd5afce2d83
starl441 <shivashankar521998@gmail.com>
2024-03-02 19:29:48 +0530

    frontend integration & api calling

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ const morgan = require("morgan");
 const userrouter=require('./routes/userroutes')
 const productrouter=require('./routes/productroutes');
 const server = express();
-
-
-
-
+const cors=require("cors")
+const path=require('path')
 
 server.use(express.json());
 server.use(morgan("dev"));
+server.use(cors())
 server.use(express.static(process.env.PUBLIC_DIR))
 server.use('/products',productrouter.router);
 server.use('/users',userrouter.router);
+server.use('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,build,index.html))
+})
+
+
+
+
+
 
 console.log(process.env.PUBLIC,typeof(process.env.PUBLIC))
 server.listen(process.env.PORT,()=>{
