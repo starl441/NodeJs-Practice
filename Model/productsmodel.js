ad41ea1b4892a40ba1cde1f8c498a055d8ab888b
starl441 <shivashankar521998@gmail.com>
2024-03-02 20:05:27 +0530

    added build and run succesffully

--- Model/productsmodel.js
+++ Model/productsmodel.js
@@ -5,7 +5,7 @@ main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect(
-    "mongodb+srv://shiva:vVAMjeZVeMr4OLQJ@cluster0.vkcd0dl.mongodb.net/ecommerce"
+    process.env.MONGODB
   );
   console.log("Datbase Connected");
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
